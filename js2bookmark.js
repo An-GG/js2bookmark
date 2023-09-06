f4134332256d71cfb2749084b8da9d7241f2ac91
Ankush Girotra <ankush.girotra@gmail.com>
2023-09-06 06:42:59 -0500

    feature

--- js2bookmark.js
+++ js2bookmark.js
@@ -1,59 +1,42 @@
 #!/usr/bin/env node
 
-function scriptLoader(code, path, isJQuery) {
-  return (
-    "" +
-    "function callback(){" +
-    (isJQuery ? "(function($){var jQuery=$;" : "") +
-    code +
-    (isJQuery ? "})(jQuery.noConflict(true))" : "") +
-    "}" +
-    'var s=document.createElement("script");' +
-    's.src="' +
-    path +
-    '";' +
-    "if(s.addEventListener){" +
-    's.addEventListener("load",callback,false)' +
-    "}else if(s.readyState){" +
-    "s.onreadystatechange=callback" +
-    "}" +
-    "document.body.appendChild(s);"
-  );
+function asBookmarklet(code_array) {
+	code = ""
+	for (let text of code_array) {
+		code = code + "(function(){" + text + "})();";
+	}
+
+		code = "(function(){" + code + "})();";
+	return "javascript:" + encodeURIComponent(code);
 }
 
-function asBookmarklet(code, jQueryPath, customPath) {
-  if (customPath) {
-    code = scriptLoader(code, customPath, false);
-  }
 
-  if (jQueryPath) {
-    code = scriptLoader(code, jQueryPath, true);
-  }
+function main() {
+	let args=JSON.parse(JSON.stringify(process.argv))
+	args=args.slice(2)
 
-  code = "(function(){" + code + "})()";
-  return "javascript:" + encodeURIComponent(code);
+	let usage=`
+	USAGE:
+
+		js2bookmark.js <a.js> <b.js> ...
+
+			INPUT : path to the JavaScript file(s) you want to inject into the browser when clicking the bookmark.
+
+			OUTPUT : raw text of bookmark link. code from each file will be encapsulated in its own wrapper function.
+	`
+
+	if (args.includes('-h') || args.includes('--help') || args.length < 1) {
+		console.log(usage);
+		process.exit(1);
+	}
+
+	let fs=require('fs')
+	let code_array = []
+	for (let a of args) {
+		let script_text=fs.readFileSync(a).toString()
+		code_array.push(script_text)
+	}
+	console.log(asBookmarklet(code_array))
 }
 
-let jqpath='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'
-
-let args=JSON.parse(JSON.stringify(process.argv))
-args=args.slice(2)
-
-let usage=`
-USAGE:
-
-        js2bookmark.js <input.js>
-
-		<input.js> : path to the JavaScript file you want to inject into the browser when clicking the bookmark.
-
-	output: raw text of bookmark link
-`
-
-if (args.includes('-h') || args.includes('--help') || args.length < 1) {
-	console.log(usage);
-	process.exit(1);
-}
-
-let fs=require('fs')
-let script_text=fs.readFileSync(args[0]).toString()
-console.log(asBookmarklet(script_text))
+main()
